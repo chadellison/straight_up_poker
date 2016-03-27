1b2e124568590879386c8f5ce563d7673d906761
Chad Ellison <chad.ellison0123@gmail.com>
2016-03-27 00:17:32 -0600

    Refactor same kind of hand functionality

--- app/models/card_analyzer.rb
+++ app/models/card_analyzer.rb
@@ -82,6 +82,7 @@ class StraightFlush
 end
 
 class Straight
+  include CardHelper
   attr_reader :cards
 
   def initialize(cards)
@@ -95,7 +96,7 @@ class Straight
     end.map(&:value).uniq
 
     while sorted_values.size > 0 do
-      if sorted_values[1].nil? || sorted_values.first.to_i + 1 == sorted_values[1].to_i
+      if sorted_values[1].nil? || sorted_values.first + 1 == sorted_values[1]
         straight << sorted_values.first
       else
         straight = [] if straight.size < 4
@@ -103,31 +104,6 @@ class Straight
       sorted_values.shift
     end
     straight.size > 4
-  end
-
-  def card_converter(cards)
-    cards.map do |card|
-      case card.value
-      when "Ace"
-        ace_low? ? Card.new(1, card.suit) : Card.new(14, card.suit)
-      when "King"
-        Card.new(13, card.suit)
-      when "Queen"
-        Card.new(12, card.suit)
-      when "Jack"
-        Card.new(11, card.suit)
-      else
-        card
-      end
-    end
-  end
-
-  def ace_low?
-    card_values = cards.map { |card| card.value.to_i }
-    [card_values.include?(2),
-      card_values.include?(3),
-      card_values.include?(4),
-      card_values.include?(5)].all?
   end
 end
 
@@ -173,6 +149,7 @@ class TwoPair
 end
 
 class CardAnalyzer
+  include CardHelper
   # the order of this collection is important. it is in order by hands' values
   HANDS = [RoyalFlush, StraightFlush, FourOfKind, FullHouse, Flush, Straight, ThreeOfKind, TwoPair, TwoOfKind, HighCard]
 
@@ -197,7 +174,7 @@ class CardAnalyzer
       best_hand.first.first.take_winnings
     else
       best_hand.map do |player_hand|
-        [player_hand.first, find_best(player_hand.last)] #sort_by value here
+        [player_hand.first, find_best(player_hand.last)]
       end.sort_by do |best_cards|
         best_cards.last.map(&:value)
       end.last.first.take_winnings #this needs to handle ties
@@ -206,49 +183,5 @@ class CardAnalyzer
 
   def index_hand(cards)
     HANDS.index(find_hand(cards).class)
-  end
-
-  def find_best(cards)
-    cards = card_converter(cards).sort_by(&:value)
-    hand = find_hand(cards).class
-    return [cards.last] if hand == HighCard
-    best_cards = cards.select do |card|
-      find_hand(cards.reject do |c|
-        c == card
-      end).class != hand
-    end.reverse
-    remaining = cards.reject { |card| best_cards.map(&:value).include?(card.value)}.reverse
-    (best_cards + remaining)[0..4]
-  end
-
-  def make_card_objects(cards)
-    cards.map do |card|
-      Card.new(card.split.first, card.split.last)
-    end
-  end
-
-  def card_converter(cards)
-    cards.map do |card|
-      case card.value
-      when "Ace"
-        ace_low?(cards) ? Card.new(1, card.suit) : Card.new(14, card.suit)
-      when "King"
-        Card.new(13, card.suit)
-      when "Queen"
-        Card.new(12, card.suit)
-      when "Jack"
-        Card.new(11, card.suit)
-      else
-        Card.new(card.value.to_i, card.suit)
-      end
-    end
-  end
-
-  def ace_low?(cards)
-    card_values = cards.map { |card| card.value.to_i }
-    [card_values.include?(2),
-      card_values.include?(3),
-      card_values.include?(4),
-      card_values.include?(5)].all?
   end
 end
